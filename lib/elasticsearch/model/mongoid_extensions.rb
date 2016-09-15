a87e7aa6d26738ecc26f4a40c81c57e6c49e16e7
Tomas Celizna <tomas.celizna@gmail.com>
2016-09-15 15:52:49 +0200

    remove

--- lib/elasticsearch/model/mongoid_extensions.rb
+++ lib/elasticsearch/model/mongoid_extensions.rb
@@ -9,14 +9,3 @@ require 'elasticsearch/model/mongoid_extensions/localized'
 require 'elasticsearch/model/mongoid_extensions/sci'
 
 require 'elasticsearch/model/mongoid_extensions/version'
-
-module Elasticsearch
-  module Model
-    module MongoidExtensions
-      # always exclude id fields from being serialised
-      def as_indexed_json(options = {})
-        as_json options.deep_merge(except: %i(id _id))
-      end
-    end
-  end
-end
