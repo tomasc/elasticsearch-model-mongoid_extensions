2cf51b2a91f043a16bed45baf3d59748b3012e41
Tomas Celizna <tomas.celizna@gmail.com>
2016-09-15 12:35:50 +0200

    refactor

--- lib/elasticsearch/model/mongoid_extensions/localized.rb
+++ lib/elasticsearch/model/mongoid_extensions/localized.rb
@@ -25,31 +25,35 @@ module Elasticsearch
           end
         end
 
-        included do
-          include Elasticsearch::Model
-          include Elasticsearch::Model::MongoidExtensions
-
-          def self.mapping(options = {}, &block)
-            options[:cls] ||= self # pass the class to the Mappings object
-            __elasticsearch__.instance_variable_set(:@mapping, Mappings.new(document_type, options)) unless __elasticsearch__.instance_variable_get(:@mapping)
-            __elasticsearch__.mapping(options, &block)
-          end
-
-          # wraps the #as_indexed_json method in order to process the localized fields
-          proxy = Module.new do
-            define_method(:as_indexed_json) do |*args|
-              super(*args).tap do |obj|
-                fields.select { |_, field| field.localized? }.each do |name, field|
-                  aliased_name = field.options[:as].to_s
-                  translations_field_name = [name, 'translations'].join('_')
-                  if obj.key?(name) then obj[name] = send(translations_field_name)
-                  elsif obj.key?(aliased_name) then obj[aliased_name] = send(translations_field_name)
-                  end
+        module Serializing
+          def as_indexed_json(*args)
+            super(*args).tap do |obj|
+              fields.select { |_, field| field.localized? }.each do |name, field|
+                aliased_name = field.options[:as].to_s
+                translations_field_name = [name, 'translations'].join('_')
+                if obj.key?(name) then obj[name] = send(translations_field_name)
+                elsif obj.key?(aliased_name) then obj[aliased_name] = send(translations_field_name)
                 end
               end
             end
           end
-          prepend proxy
+        end
+
+        included do
+          include Elasticsearch::Model::MongoidExtensions
+
+          # FIXME: not so clean to override the mapping method on Elasticsearch::Model::Indexing::Mappings directly
+          def self.mapping(options = {}, &block)
+            @mapping ||= Mappings.new(document_type, { cls: self }.merge(options))
+            @mapping.options.update(options) unless options.empty?
+
+            return @mapping unless block_given?
+
+            @mapping.instance_eval(&block)
+            self
+          end
+
+          prepend Serializing
         end
       end
     end
