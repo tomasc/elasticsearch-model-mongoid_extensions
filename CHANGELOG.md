e2620c2c71b3acbcfdf4dd7333981b968df90f9c
Tomáš Celizna <tomas.celizna@gmail.com>
2017-08-20 21:17:17 +0200

    1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 1.0.0
+
+* upgrade to `elasticsearch-rails`, `~> 5.0`
--- lib/elasticsearch/model/mongoid_extensions/version.rb
+++ lib/elasticsearch/model/mongoid_extensions/version.rb
@@ -1,7 +1,7 @@
 module Elasticsearch
   module Model
     module MongoidExtensions
-      VERSION = '0.1.5'.freeze
+      VERSION = '1.0.0'.freeze
     end
   end
 end
